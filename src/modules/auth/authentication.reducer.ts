98ff37a0e1da1ee6bb82398a1e62167980bdd95b
TriNguyen <nttri2k2@gmail.com>
2024-12-01 16:30:35 +0700

    small update

--- src/modules/auth/authentication.reducer.ts
+++ src/modules/auth/authentication.reducer.ts
@@ -115,6 +115,6 @@ export const AuthSlice = createSlice({
   },
 });
 
-export const { clearAuth, logoutSession,resetLoginError  } = AuthSlice.actions;
+export const { clearAuth, logoutSession, resetLoginError } = AuthSlice.actions;
 
 export default AuthSlice.reducer;
